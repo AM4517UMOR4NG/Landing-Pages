35af91917dcddc952f60ffa3b2c87c0d799ffcc4
Alogo Situmorang <aekmohop@gmail.com>
2025-10-27 15:13:25 +0700

    Push app jsx as a main home to run this project

---
+++ Landing_Pages_ElBengkel/src/App.jsx
@@ -0,0 +1,33 @@
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Services from './components/Services'
+import Features3D from './components/Features3D'
+import InteractiveShowcase from './components/InteractiveShowcase'
+import Advertisement from './components/Advertisement'
+import ParallaxSection from './components/ParallaxSection'
+import PhotoGallery from './components/PhotoGallery'
+import Stats from './components/Stats'
+import Testimonials from './components/Testimonials'
+import CTA from './components/CTA'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Header />
+      <Hero />
+      <Services />
+      <Features3D />
+      <InteractiveShowcase />
+      <Advertisement />
+      <ParallaxSection />
+      <PhotoGallery />
+      <Stats />
+      <Testimonials />
+      <CTA />
+      <Contact />
+      <Footer />
+    </div>
+  )
+}
