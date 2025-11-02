e35b4da70fd710bd6e2ec25e3d90c26e33bb0991
Alogo Situmorang <aekmohop@gmail.com>
2025-11-02 17:33:03 +0700

    [ush service jsx

--- Landing_Pages_ElBengkel/src/components/Services.jsx
+++ Landing_Pages_ElBengkel/src/components/Services.jsx
@@ -68,7 +68,7 @@ export default function Services() {
   }
 
   return (
-    <section id="services" className="py-20 bg-gray-50">
+    <section id="services" className="-mt-px py-20 bg-gray-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Section Header */}
         <motion.div
