06a39eb6ac71ea7fd0b5200e2ed44b9492922564
Alogo Situmorang <aekmohop@gmail.com>
2025-11-02 17:32:14 +0700

    push app jsx

--- Landing_Pages_ElBengkel/src/App.jsx
+++ Landing_Pages_ElBengkel/src/App.jsx
@@ -14,7 +14,7 @@ import Footer from './components/Footer'
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-black">
       <Header />
       <Hero />
       <Services />
